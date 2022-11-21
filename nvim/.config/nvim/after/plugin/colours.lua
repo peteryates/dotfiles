e8c97c79916a9bb8ea4cb21977e3cb492da0e77c
Peter Yates <peter.yates@graphia.co.uk>
2022-11-21 18:05:14 +0000

    Add highlight for TextYankPost

--- nvim/.config/nvim/after/plugin/colours.lua
+++ nvim/.config/nvim/after/plugin/colours.lua
@@ -3,3 +3,10 @@ vim.opt.background = 'dark'
 
 vim.cmd('colorscheme dracula')
 vim.cmd('highlight WinSeparator guibg=None guifg=#44475a')
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function(_)
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 75 })
+  end,
+})
