90ff15a7f572c13eee3637134af50aca5a35adf2
Peter Yates <peter.yates@graphia.co.uk>
2023-10-02 14:39:32 +0100

    Update settings for md codeblocks and frontmatter

--- nvim/.config/nvim/lua/wiki.lua
+++ nvim/.config/nvim/lua/wiki.lua
@@ -29,7 +29,8 @@ vim.api.nvim_set_keymap(
 
 vim.g.lists_filetypes = {'md', 'markdown'}
 vim.g.vim_markdown_strikethrough = 1
-vim.g.vim_markdown_conceal_code_blocks = 0
+vim.g.vim_markdown_conceal_code_blocks = 1
 vim.g.vim_markdown_new_list_item_indent = 2
 vim.g.vim_markdown_auto_insert_bullets = 0
 vim.g.vim_markdown_new_list_item_indent = 0
+vim.g.vim_markdown_frontmatter = 1
