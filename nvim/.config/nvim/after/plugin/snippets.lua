b88ad5e0cacb7c26f5c4e712bee2dd1a0400d37e
Peter Yates <peter.yates@graphia.co.uk>
2024-04-03 17:04:22 +0100

    Add Claire to co-authors snippets

--- nvim/.config/nvim/after/plugin/snippets.lua
+++ nvim/.config/nvim/after/plugin/snippets.lua
@@ -49,16 +49,17 @@ ls.add_snippets(
 ls.add_snippets(
     "gitcommit",
     {
-        s("ellie", t("Co-authored-by: EllieNodder <94541493+EllieNodder@users.noreply.github.com>")),
-        s("gemma", t("Co-authored-by: gemmadallmandfe <87642394+gemmadallmandfe@users.noreply.github.com>")),
-        s("hazel", t("Co-authored-by: hazelyule <111048910+hazelyule@users.noreply.github.com>")),
-        s("tony",  t("Co-authored-by: tonyheadford <tony@binarycircus.com>")),
-        s("abeer", t("Co-authored-by: cwrw <cwrw@users.noreply.github.com>")),
-        s("naomi", t("Co-authored-by: Lockyy <naomi@lockyy.com>")),
+        s("ellie",   t("Co-authored-by: EllieNodder <94541493+EllieNodder@users.noreply.github.com>")),
+        s("gemma",   t("Co-authored-by: gemmadallmandfe <87642394+gemmadallmandfe@users.noreply.github.com>")),
+        s("hazel",   t("Co-authored-by: hazelyule <111048910+hazelyule@users.noreply.github.com>")),
+        s("tony",    t("Co-authored-by: tonyheadford <tony@binarycircus.com>")),
+        s("abeer",   t("Co-authored-by: cwrw <cwrw@users.noreply.github.com>")),
+        s("naomi",   t("Co-authored-by: Lockyy <naomi@lockyy.com>")),
         s("frankie", t("Co-authored-by: Frankie Roberto <frankie@frankieroberto.com>")),
-        s("rick",  t("Co-authored-by: Rick McVelia <rick.mcvelia@gmail.com>")),
-        s("pablo",  t("Co-authored-by: Pablo Manrubia <pmanrubia@gmail.com>")),
-        s("leo",  t("Co-authored-by: Leonidas Apostolidis <leoapost@users.noreply.github.com>")),
+        s("rick",    t("Co-authored-by: Rick McVelia <rick.mcvelia@gmail.com>")),
+        s("pablo",   t("Co-authored-by: Pablo Manrubia <pmanrubia@gmail.com>")),
+        s("leo",     t("Co-authored-by: Leonidas Apostolidis <leoapost@users.noreply.github.com>")),
+        s("claire",  t("Co-authored-by: claire-hughez <93539621+claire-hughez@users.noreply.github.com>")),
     }
 )
 
