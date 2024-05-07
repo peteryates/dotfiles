bd899de2a960aad266f39be54b78bdbdeeefd154
Peter Yates <peter.yates@graphia.co.uk>
2024-05-07 18:14:13 +0100

    Remove packer step from Makefile
    Also make Lazy install itself if it's not already there

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,17 @@
 vim.cmd('so /usr/share/nvim/site/plugin/fzf.vim')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
