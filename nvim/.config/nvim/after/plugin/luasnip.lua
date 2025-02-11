4aab99d4029d130d8f24383c8a8e556cfdfc2540
Peter Yates <peter.yates@graphia.co.uk>
2025-02-11 00:01:39 +0000

    Replace blink and revert to basic omnifunc lsp

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -81,3 +81,15 @@ ls.add_snippets(
     ),
   }
 )
+
+vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
---
+++ nvim/.config/nvim/lua/custom/plugins/luasnip.lua
@@ -0,0 +1,5 @@
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  dependencies = { 'rafamadriz/friendly-snippets' }
+}
