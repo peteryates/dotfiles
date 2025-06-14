19ae6e6d4aecdcfcc844d24578aec758368a05e0
Peter Yates <peter.yates@graphia.co.uk>
2025-06-14 20:37:28 +0100

    Update luasnip bindings and add git hunk jumping

--- nvim/.config/nvim/lua/custom/plugins/fzf.lua
+++ nvim/.config/nvim/lua/custom/plugins/fzf.lua
@@ -7,17 +7,53 @@ return {
         file_icons = false,
         fzf_opts =  {['--layout'] = 'default'}
       })
-      vim.keymap.set({'n'}, '<Leader>t', ':FzfLua files<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>T', ':FzfLua grep<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>b', ':FzfLua buffers<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>l', ':FzfLua blines<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>L', ':FzfLua lines<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>h', ':FzfLua git_bcommits<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>H', ':FzfLua git_commits<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>s', ':FzfLua spell_suggest<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>r', ':FzfLua lsp_references<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>g', ':FzfLua grep_project<CR>', {expr = false, noremap = true, silent = true})
-      vim.keymap.set({'n'}, '<Leader>G', ':FzfLua grep_cWORD<CR>', {expr = false, noremap = true, silent = true})
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>t",
+        function() require("fzf-lua").files() end,
+        { silent = true, desc = "Fuzzy find files" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>T",
+        function() require("fzf-lua").live_grep() end,
+        { silent = true, desc = "Live grep" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>b",
+        function() require("fzf-lua").buffers() end,
+        { silent = true, desc = "Fuzzy find open buffers" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>l",
+        function() require("fzf-lua").blines() end,
+        { silent = true, desc = "Fuzzy find lines in current buffer" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>L",
+        function() require("fzf-lua").blines() end,
+        { silent = true, desc = "Fuzzy find lines in all buffers" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>h",
+        function() require("fzf-lua").git_bcommits() end,
+        { silent = true, desc = "Fuzzy find buffer git commits" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>H",
+        function() require("fzf-lua").git_commits() end,
+        { silent = true, desc = "Fuzzy find project git commits" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>s",
+        function() require("fzf-lua").spell_suggest() end,
+        { silent = true, desc = "Suggest spellings" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>r",
+        function() require("fzf-lua").lsp_references() end,
+        { silent = true, desc = "Fuzzy find LSP references" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>d",
+        function() require("fzf-lua").diagnostics_document() end,
+        { silent = true, desc = "Fuzzy find diagnostics in buffer" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<Leader>D",
+        function() require("fzf-lua").diagnostics_document() end,
+        { silent = true, desc = "Fuzzy find diagnostics in project" })
+
+      vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
+        function() require("fzf-lua").complete_path() end,
+        { silent = true, desc = "Fuzzy complete path" })
     end
   }
 }
--- nvim/.config/nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/custom/plugins/gitsigns.lua
@@ -10,6 +10,30 @@ return {
         delay = 1500,
         ignore_whitespace = true,
       },
+      on_attach = function(bufnr)
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        local gitsigns = require('gitsigns')
+        map('n', ']h', function()
+          if vim.wo.diff then
+            vim.cmd.normal({']h', bang = true})
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[h', function()
+          if vim.wo.diff then
+            vim.cmd.normal({'[h', bang = true})
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+      end
     }
   end
 }
