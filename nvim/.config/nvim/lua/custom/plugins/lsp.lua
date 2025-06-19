c20d7725b8a3f493e5519c05b7016477e47268ad
Peter Yates <peter.yates@graphia.co.uk>
2025-06-19 10:32:08 +0100

    Add yamllls settings

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -46,5 +46,16 @@ return {
     --     }
     --   }
     -- })
+    lspconfig.yamlls.setup({
+      settings = {
+        yaml = {
+          schemas = {
+            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            ["../path/relative/to/file.yml"] = "/.github/workflows/*",
+            ["/path/from/root/of/project"] = "/.github/workflows/*",
+          }
+        }
+      }
+    })
   end
 }
