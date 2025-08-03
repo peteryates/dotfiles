5531398b387d9944edbc4e32d3f8cf94160c0341
Peter Yates <peter.yates@graphia.co.uk>
2025-08-03 10:50:13 +0100

    Darken terminal background a touch

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -8,6 +8,7 @@ end
 # variables' info in temporary files and pass in the filenames as arguments.
 # This variable is global so that it can be referenced by fzf_configure_bindings and in tests
 set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub) (set --names | psub)'
+set --global FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#161d1c,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
 
 # Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
