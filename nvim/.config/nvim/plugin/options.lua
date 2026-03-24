e84e65adf6bcde4254d74a6deaf4a2a1239fa4fd
Peter Yates <peter.yates@graphia.co.uk>
2026-03-24 13:37:53 +0000

    Disable smartindent as it's interfering with TS

--- nvim/.config/nvim/plugin/options.lua
+++ nvim/.config/nvim/plugin/options.lua
@@ -20,7 +20,7 @@ vim.opt.virtualedit = 'none'
 vim.opt.mouse = ''
 vim.opt.conceallevel = 3
 vim.opt.cursorline = false
-vim.opt.smartindent = true
+-- vim.opt.smartindent = true
 vim.opt.winborder = 'rounded'
 
 -- search
