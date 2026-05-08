85907962f1d90c7653c2349aaea8d19ea1c72e7a
Peter Yates <peter.yates@graphia.co.uk>
2026-05-08 17:11:44 +0100

    Update Fish config for Fish 4.0

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,34 +1,24 @@
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+set -g __fish_git_prompt_showupstream informative
+set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_use_informative_chars 1
+set -g __fish_git_prompt_char_dirtystate "◍"
+set -g __fish_git_prompt_char_untrackedfiles "○"
+set -g fish_color_vcs cyan
+
 function fish_prompt
         set -l last_status $status
     
         set -l normal (set_color normal)
         set -l usercolor (set_color $fish_color_user)
     
-        set -l delim ' $ '
+        set -l delim '$ '
     
         fish_is_root_user; and set delim "#"
     
         set -l cwd (set_color $fish_color_cwd)
-        if command -sq cksum
-                # randomized cwd color
-                # We hash the physical PWD and turn that into a color. That means directories (usually) get different colors,
-                # but every directory always gets the same color. It's deterministic.
-                # We use cksum because 1. it's fast, 2. it's in POSIX, so it should be available everywhere.
-                set -l shas (pwd -P | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 | string pad -c 0 -w 6 | string match -ra ..)
-                set -l col 0x$shas[1..3]
-        
-                # If the (simplified idea of) luminance is below 120 (out of 255), add some more.
-                # (this runs at most twice because we add 60)
-                while test (math 0.2126 x $col[1] + 0.7152 x $col[2] + 0.0722 x $col[3]) -lt 120
-                        set col[1] (math --base=hex "min(255, $col[1] + 60)")
-                        set col[2] (math --base=hex "min(255, $col[2] + 60)")
-                        set col[3] (math --base=hex "min(255, $col[3] + 60)")
-                end
-                set -l col (string replace 0x '' $col | string pad -c 0 -w 2 | string join "")
-        
-                set cwd (set_color $col)
-        end
-    
+
         # Prompt status only if it's not 0
         set -l prompt_status
         test $last_status -ne 0; and set prompt_status (set_color $fish_color_status)"[$last_status]$normal"
@@ -46,8 +36,10 @@ function fish_prompt
                 end
         end
     
+        set -l vcs (set_color normal)(fish_vcs_prompt)
+    
         # Shorten pwd if prompt is too long
         set -l pwd (prompt_pwd)
     
-        echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim
+        echo -n -s $prompt_host $cwd $pwd $vcs \n $normal $prompt_status $delim
 end
